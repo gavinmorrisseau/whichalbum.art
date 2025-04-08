07c85be3228691a4454ca26a6d27800d46286149
Gavin Morrisseau <81901485+gavinmorrisseau@users.noreply.github.com>
2025-04-08 00:03:45 +0000

    Add ability to get two (guaranteed) unique album images and return as URLs. Update README.

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-# cover-art-contest
-Web page to compare music's many cover arts.
+# whichalbum.art
+Album art compared in a versus format.
 
-
-Requirements:
+# Requirements
 pip3 install python3-discogs-client
 pip3 install iGetMusic
+
+# Project To-Do List
+
+- Add comparison functionality
+- Create main web page
+- Use Django framework to create webpage
+- Add ELO / ranking system for art based on user votess
+- - Add list of global rankings to see the top art
+- Enlarge database of album titles
+- Switch or change image getter (currently, album must have a song with the album's title to return valid)
--- main.py
+++ main.py
@@ -9,15 +9,53 @@ def getImage(songName: str) -> str:
     imageURL = iGet.resizeImage(imageURL,1000)
     return imageURL
 
-def randomTitle() -> str:
-    index = random.randint(1,5000)
+
+def getTitleFromIndex(index):
     file = open(PATH_TO_FILE,"r",encoding="UTF-8")
     for i, line in enumerate(file):
         if i == index:
-            _, album_title, album_artist, *_ = line.split(",")
+            return line
         elif i > index:
             break
+    file.close()
+    return str()
+
+def getRandomAlbumTitle() -> str:
+    index = random.randint(1,5000)
+    _, album_title, album_artist, *_ = getTitleFromIndex(index).split(",")
     print(f'randomTitle() returns: {album_title} {album_artist}')
-    return f'{album_title} {album_artist}'\
+    return f'{album_title} {album_artist}'
         
-print(getImage(randomTitle()))
+def getRandomAlbumTitles() -> list:
+    outputList = list()
+    index1 = random.randint(1,5000)
+    index2 = random.randint(1,5000)
+    
+    while index1 == index2:
+        index2 = random.randint(1,5000)
+
+    _, album_title1, album_artist1, *_ = getTitleFromIndex(index1).split(",")
+    _, album_title2, album_artist2, *_ = getTitleFromIndex(index2).split(",")
+
+    outputList.append(f'{album_title1} {album_artist1}')
+    outputList.append(f'{album_title2} {album_artist2}')
+    return outputList
+
+initialAlbums = getRandomAlbumTitles()
+
+
+album1 = None
+album2 = None
+
+while album1 == None or album2 == None:
+    try:
+        album1 = getImage(initialAlbums[0])
+        album2 = getImage(initialAlbums[1])
+    except Exception as e:
+        print(f'Error retrieving image for {initialAlbums[0]}: {e}')
+        initialAlbums = getRandomAlbumTitles()
+
+print(initialAlbums[0])
+print(initialAlbums[1])
+print(album1)
+print(album2)
